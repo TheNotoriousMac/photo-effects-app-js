e0f64927c72ad8c1a5ae64512441baee23ed7183
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-10-05 20:30:09 -0400

    Added functionality to upload any image to canvas.

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-img.src = "images/bridge.jpg";
-img.onload =  () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-};
 
-//
+const reader = new FileReader();
+
+function uploadImage(e) {
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+        img.src = reader.result;
+        img.onload =  () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        };
+    }
+}
+
+const imageLoader = document.getElementById("uploader");
+imageLoader.addEventListener("change", uploadImage);
+
+
